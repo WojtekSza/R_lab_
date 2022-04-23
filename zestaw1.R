c926dc301b8aeb1ffcede5f2cdb6f2b9a6732735
WojtekSza <wojciech.szaniawski1@gmail.com>
2022-04-23 14:43:37 +0200

    final

--- zestaw1.R
+++ zestaw1.R
@@ -2,72 +2,74 @@
 getwd()
 df <- read.csv("autaSmall.csv")
 head(df,5)
-
 #2 Pobranie danych z REST API
 
 install.packages("jsonline")
 install.packages("httr")
 library(jsonlite)
-require(httr)
+library(httr)
 
 endpoint <- "https://api.openweathermap.org/data/2.5/weather?q=Warszawa&appid=1765994b51ed366c506d5dc0d0b07b77"
 response <- GET(endpoint)
 weather <- as.data.frame(fromJSON(endpoint))
 View(weather)
 
-#3 Funkcja zapisujaca porcjami danych csv do tabeli SQLite
-?read.table
 
+#3 Funkcja zapisujaca porcjami danych csv do tabeli SQLite
 install.packages("DBI")
 install.packages("RSQLite")
 
 library(DBI)
 library(RSQLite)
 
-?file
-
-
-readToBase<-function(filepath,dbConn,tablename,size,sep=",",header=TRUE,delete=TRUE, encoding="UTF-8"){
-  ap=!delete
-  ov=delete
-
-fileCon <- file(description = filepath, open = "r",encoding = encoding)
-
-df1 <- read.table(fileCon,header=TRUE,sep=",", fill=TRUE,
-                  fileEncoding =  'UTF-8', nrows = size)
-
-myColNames <- names(df1)
-dbWriteTable(con,tablename,df1,append=ap,overwrite=ov)
-
-i <- 10
-repeat {
-if(nrow(df1)==0) {
-  break;
-  close(fileCon)
-  dbDisconnect(con)
+readToBase<-function(filepath,dbConn,tablename,size,sep=",",header=TRUE,delete=TRUE){
+  ap<- !delete
+  ov<- delete
+  fileConnection<-file(description = filepath,open="r")
+  df<- read.table(fileConnection,nrows = size,header = header,fill=TRUE,sep=sep) 
+  dbWriteTable(con, tablename, df,append=ap,overwrite=ov)
+  myColNames<- names(df)
+  repeat{
+    if( nrow(df)==0){
+      dbDisconnect(con)
+      close(fileConnection)
+      break 
+    }
+    df<- read.table(fileConnection,nrows = size,col.names = myColNames,fill=TRUE,sep=sep)
+    dbWriteTable(con,tablename, df,append=TRUE,overwrite=FALSE)
   }
-df1 <- read.table(fileCon,col.names = myColNames,sep=",",
-                  fileEncoding = encoding, nrows = 90)
-dbWriteTable(con,"tabela",df1,append=TRUE,overwrite=FALSE)
-print(i)
-i<- i +90
-}
-}
-# i <- 1
-# repeat{
-#   if(i>5) {
-#     break}
-#   print(i)
-# i<- i +1
-#   }
-
-
-View(df1)
-
-
+} 
 con <- dbConnect(SQLite(),"auta.sqlite")
-
-readToBase("autaSmall.csv",com,"auta2",1000)
+readToBase("autaSmall.csv",con,"auta",1000)
 dbDisconnect(con)
 close(fileCon)
+
+#4.Napisz funkcję znajdującą tydzień obserwacji z największą średnią ceną ofert korzystając z zapytania SQL.
+con <- dbConnect(SQLite(),"auta.sqlite")
+res<-dbSendQuery(con,"SELECT tydzien from auta GROUP by tydzien ORDER by avg(cena) DESC limit 1")
+zBazy<-dbFetch(res)
+dbClearResult(res)
+dbDisconnect(con)
+
+#5. Podobnie jak w poprzednim zadaniu napisz funkcję znajdującą tydzień obserwacji z największą 
+#średnią ceną ofert  tym razem wykorzystując REST api.
+
+readtoBaseAPI <- function(api,con,tablename) {
+  i<-1
+  week<-fromJSON(paste(api,i,sep=""))
+  dbWriteTable(con,tablename, week,append=FALSE,overwrite=TRUE)
+  
+  for (i in 2:nweeks$`MAX(tydzien)`) {
+    print(i)
+    week<-fromJSON(paste(api,i,sep=""))
+    dbWriteTable(con,tablename, week,append=TRUE,overwrite=FALSE)  
+  }
+}
+nweeks<-fromJSON("http://54.37.136.190:8000/nweek")
+con <- dbConnect(SQLite(),"auta.sqlite")
+readtoBaseAPI("http://54.37.136.190:8000/week?t=",con,"auta_API")
+res<-dbSendQuery(con,"SELECT tydzien from auta_API GROUP by tydzien ORDER by avg(cena) DESC limit 1")
+zBazyAPI<-dbFetch(res)
+dbClearResult(res)
+dbDisconnect(con)
 
